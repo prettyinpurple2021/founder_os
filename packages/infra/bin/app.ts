4ebd4c763311b401778e1187796c2df0a856ca8c
Frances Loggins <124447531+prettyinpurple2021@users.noreply.github.com>
2026-06-22 08:10:50 -0400

    Update app.ts

--- packages/infra/bin/app.ts
+++ packages/infra/bin/app.ts
@@ -4,6 +4,13 @@ import * as cdk from 'aws-cdk-lib';
 import { environments } from '../lib/config/environments.js';
 import { applyTags } from '../lib/config/tags.js';
 
+// Import your infrastructure stacks
+import { NetworkStack } from '../lib/stacks/network-stack.js';
+import { DatabaseStack } from '../lib/stacks/database-stack.js';
+import { ContainerStack } from '../lib/stacks/container-stack.js';
+import { CdnStack } from '../lib/stacks/cdn-stack.js';
+import { MonitoringStack } from '../lib/stacks/monitoring-stack.js';
+
 const app = new cdk.App();
 
 const stage = app.node.tryGetContext('stage') as 'staging' | 'production' ?? 'staging';
@@ -12,12 +19,11 @@ const config = environments[stage];
 // Apply global tags to all resources in the app
 applyTags(app, config.stage);
 
-// Stacks will be added here as they are implemented
-// Example:
-// new NetworkStack(app, `${config.stage}-network`, { env: { account: config.account, region: config.region }, config });
-// new DatabaseStack(app, `${config.stage}-database`, { env: { account: config.account, region: config.region }, config });
-// new ContainerStack(app, `${config.stage}-container`, { env: { account: config.account, region: config.region }, config });
-// new CdnStack(app, `${config.stage}-cdn`, { env: { account: config.account, region: config.region }, config });
-// new MonitoringStack(app, `${config.stage}-monitoring`, { env: { account: config.account, region: config.region }, config });
+// Activate the stacks
+new NetworkStack(app, `${config.stage}-network`, { env: { account: config.account, region: config.region }, config });
+new DatabaseStack(app, `${config.stage}-database`, { env: { account: config.account, region: config.region }, config });
+new ContainerStack(app, `${config.stage}-container`, { env: { account: config.account, region: config.region }, config });
+new CdnStack(app, `${config.stage}-cdn`, { env: { account: config.account, region: config.region }, config });
+new MonitoringStack(app, `${config.stage}-monitoring`, { env: { account: config.account, region: config.region }, config });
 
 app.synth();
